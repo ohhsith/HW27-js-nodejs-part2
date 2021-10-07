abb9276f1c404842e9841cb9425fa4947e609b9e
Lera <ohhsith@ukr.net>
2021-10-07 11:54:18 +0300

    fixed

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const url = require('url');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,21 +24,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-  // res.end(console.log('hello'))
+  if (req.url == '/home') {
+    res.end("Home");
+  } else {
+    next();
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function(req, res, next) {
+  if (getAccess(req)) {
+    res.end("Access approved");
+  } else {
+  res.end("Access denied");
+  }
 });
+
+function getAccess(req) {
+    console.log(url.parse(req.url, true).query.secret)
+    return url.parse(req.url, true).query.secret === 'true';
+}
 
 module.exports = app;
